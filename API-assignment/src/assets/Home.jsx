bd6685f046a0a6b033f2526fe306053f56652633
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-12-11 09:58:37 +0100

    imported api

--- API-assignment/src/assets/Home.jsx
+++ API-assignment/src/assets/Home.jsx
@@ -1,13 +1,43 @@
-
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function Home() {
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState([]);
 
-    return (
-        <>
-        
-        </>
-    
-      )
+  useEffect(() => {
+    Axios.get("https://wizard-world-api.herokuapp.com/Houses")
+      .then((res) => {
+        setData(res.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your house"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        {data.filter((item) => {
+          return search.toLowerCase() === "" 
+          ? item 
+          : item.name.toLowerCase().includes(search);
+
+        }).map((item, index) => (
+          <ul key={index}>
+            <li>House name: {item.name}</li>
+            <li>House founder: {item.founder}</li>
+            <li>House element: {item.element}</li>
+            <li>Animal: {item.animal}</li>
+          </ul>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
